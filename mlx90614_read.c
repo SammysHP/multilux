a6840b8597571ecdb822506e71fcb762b66453ad
SammysHP <sven@sammyshp.de>
2024-10-30 19:17:44 +0100

    mlx90614_read: Parse command line options

--- mlx90614_read.c
+++ mlx90614_read.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <getopt.h>
 #include <math.h>
 #include <signal.h>
-#include <time.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
+#include <time.h>
 
 #include <hidapi.h>
 #include "cp2112.h"
@@ -23,6 +25,18 @@
 
 volatile int force_exit;
 
+struct cmdargs
+{
+    char *host;
+    int port;
+    char *username;
+    char *password;
+    char *topic;
+    double interval;
+    bool tls;
+    bool quiet;
+};
+
 void exit_handler(int sig_num)
 {
     force_exit = 1;
@@ -34,10 +48,111 @@ double compute_celsius(int n)
     return (c - 273.15) + MLX_VDD_OFFSET_DEGREES;
 }
 
+bool parse_arguments(int argc, char **argv, struct cmdargs *args)
+{
+    const static struct option long_options[] = {
+        {"host",                required_argument, 0, 'h' },
+        {"port",                required_argument, 0, 'p' },
+        {"username",            required_argument, 0, 'u' },
+        {"password",            required_argument, 0, 'w' },
+        {"topic",               required_argument, 0, 't' },
+        {"interval",            required_argument, 0, 'i' },
+        {"tls",                 no_argument,       0, 's' },
+        {"quiet",               no_argument,       0, 'q' },
+        {0, 0, 0, 0}
+    };
+
+  while (true) {
+      const int opt = getopt_long(argc, argv, "h:p:u:w:t:i:sq", long_options, NULL);
+
+      if (opt == -1) break;
+
+      switch (opt) {
+          case 'h':
+              {
+                  if (!optarg || !strlen(optarg)) return false;
+                  args->host = strdup(optarg);
+                  break;
+              }
+
+          case 'p':
+              {
+                  char *end;
+                  const long port = strtol(optarg, &end, 10);
+                  if (*end != '\0' || end == optarg || port < 0 || port > 65535) return false;
+                  args->port = port;
+                  break;
+              }
+
+          case 'u':
+              {
+                  if (!optarg || !strlen(optarg)) return false;
+                  args->username = strdup(optarg);
+                  break;
+              }
+
+          case 'w':
+              {
+                  if (!optarg || !strlen(optarg)) return false;
+                  args->password = strdup(optarg);
+                  break;
+              }
+
+          case 't':
+              {
+                  if (!optarg || !strlen(optarg)) return false;
+                  args->topic = strdup(optarg);
+                  break;
+              }
+
+          case 'i':
+              {
+                  char *end;
+                  const double interval = strtod(optarg, &end);
+                  if (*end != '\0' || end == optarg || interval <= 0) return false;
+                  args->interval = interval;
+                  break;
+              }
+
+          case 's':
+              {
+                  args->tls = true;
+                  break;
+              }
+
+          case 'q':
+              {
+                  args->quiet = true;
+                  break;
+              }
+
+          default:
+              return false;
+        }
+  }
+
+  return true;
+}
+
 int main(int argc, char *argv[])
 {
     int res, raw_ir;
-    hid_device *handle;
+
+    struct cmdargs args = {
+        .host = "",
+        .port = 1883,
+        .username = NULL,
+        .password = NULL,
+        .topic = "multilux/mlx90614",
+        .interval = 1.0,
+        .tls = false,
+        .quiet = false,
+    };
+
+    if (!parse_arguments(argc, argv, &args)) {
+        fprintf(stderr, "Failed to parse arguments.\n");
+        exit(EXIT_FAILURE);
+    }
 
     mosquitto_lib_init();
 
@@ -51,7 +166,7 @@ int main(int argc, char *argv[])
     }
 
     // serial number could be a way to handle multiple boards
-    handle = hid_open(CP2112_VID, CP2112_PID, NULL);
+    hid_device *handle = hid_open(CP2112_VID, CP2112_PID, NULL);
     if (!handle) {
         fprintf(stderr, "Unable to open CP2112.\n");
         return 1;
@@ -79,10 +194,14 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    struct mosquitto *mosq = mqtt_connect(
-        "localhost", 1883,
-        NULL, NULL,
-        0);
+    struct mosquitto *mosq = NULL;
+
+    if (*args.host) {
+        mosq = mqtt_connect(
+            args.host, args.port,
+            args.username, args.password,
+            args.tls);
+    }
 
     signal(SIGINT, exit_handler);
 
@@ -96,31 +215,31 @@ int main(int argc, char *argv[])
         gettimeofday(&tv, NULL);
         const double timestamp = tv.tv_sec + tv.tv_usec / 1e6;
 
-        printf(
-            "%.6f\t"
-            "%.2f\t"
-            "%.2f\n",
-            timestamp,
-            t_amb,
-            t_obj);
-        fflush(stdout);
+        if (!args.quiet) {
+            printf(
+                "%.6f\t%.2f\t%.2f\n",
+                timestamp, t_amb, t_obj);
+            fflush(stdout);
+        }
 
-        char mqtt_msg[MQTT_MSG_BUFLEN];
-        snprintf(mqtt_msg, MQTT_MSG_BUFLEN,
-            "{"
-                "\"timestamp\": %.6f, "
-                "\"ambient\": %.2f, "
-                "\"object\": %.2f"
-            "}",
-            timestamp,
-            t_amb,
-            t_obj);
-        mqtt_send(
-            mosq,
-            "/multilux/mlx90614",
-            strlen(mqtt_msg), mqtt_msg);
+        if (mosq) {
+            char mqtt_msg[MQTT_MSG_BUFLEN];
+            snprintf(mqtt_msg, MQTT_MSG_BUFLEN,
+                "{"
+                    "\"timestamp\": %.6f, "
+                    "\"ambient\": %.2f, "
+                    "\"object\": %.2f"
+                "}",
+                timestamp,
+                t_amb,
+                t_obj);
+            mqtt_send(
+                mosq,
+                args.topic,
+                strlen(mqtt_msg), mqtt_msg);
+        }
 
-        usleep(1 * 1000 * 1000);
+        usleep(args.interval * 1000 * 1000);
     }
 
     hid_close(handle);
